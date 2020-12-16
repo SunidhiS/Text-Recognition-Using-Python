a32ddb669a6c98feaf4b29cf84bc9520d65446da
Sunidhi Singh <sunidhi.singh0501@gmail.com>
2020-12-16 17:43:50 +0700

    Initialcommit

---
+++ test.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import pickle
+
+#####################################
+width = 640
+height = 480
+threshold = 0.65
+#####################################
+
+cap = cv2.VideoCapture(1)
+cap.set(3, width)
+cap.set(4, height)
+
+pickle_in = open("model_trained.p", "rd")
+model = pickle.load(pickle_in)
+
+def preprocessing(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    img = img/255
+    return img
+
+while True:
+    success, imgOriginal = cap.read()
+    img = np.asarray(imgOriginal)
+    img = cv2.resize(img, (320, 320))
+    img = preprocessing(img)
+    cv2.imshow("Processed Image", img)
+    img = img.reshape(1, 32, 32, 1)
+    classIndex = int(model.predict_classes(img))
+    print(classIndex)
+    predictions = model.predict(img)
+    print(predictions)
+    probVal = np.amax(predictions)
+    print(classIndex, probVal)
+
+
+if probVal> threshold:
+    cv2.putText(imgOriginal, str(classIndex) +"   " +str(probVal), (50, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (0, 0, 255), 1)
+
+    cv2.imshow("Original Image", imgOriginal)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
